fcd51816ba3bd9c51fffae1aeea7f9db9e0d1eb1
Cemal Gönültaş <45357531+cemalgnlts@users.noreply.github.com>
2023-05-19 14:31:14 +0000

    Serial0 support

--- packer/scripts/mount.sh
+++ packer/scripts/mount.sh
@@ -26,7 +26,15 @@ sudo /usr/local/sbin/grub-install --boot-directory=/mnt/sda1/boot /dev/sda
 
 echo "Create menu for GRUB"
 sudo sh -c 'echo -e "set root=(hd0,msdos1)\nset setup=(hd0)" >> /mnt/sda1/boot/grub/grub.cfg'
-sudo sh -c 'echo menuentry "Tiny Core" { >> /mnt/sda1/boot/grub/grub.cfg'
-sudo sh -c 'echo  linux /boot/vmlinuz com1=9600,8n1 loglevel=3 user=tc console=serial0 noembed nomodeset tce=sda1 opt=sda1 home=sda1 restore=sda1 >> /mnt/sda1/boot/grub/grub.cfg'
+sudo sh -c 'echo -e "set default=0\nset timeout=0" >> /mnt/sda1/boot/grub/grub.cfg'
+sudo sh -c 'echo menuentry \"Tiny Core\" { >> /mnt/sda1/boot/grub/grub.cfg'
+sudo sh -c 'echo  linux /boot/vmlinuz loglevel=3 user=tc console=tty0 console=ttyS0,115200 noembed nomodeset tce=sda1 opt=sda1 home=sda1 restore=sda1 >> /mnt/sda1/boot/grub/grub.cfg'
 sudo sh -c 'echo  initrd /boot/core.gz >> /mnt/sda1/boot/grub/grub.cfg'
 sudo sh -c 'echo } >> /mnt/sda1/boot/grub/grub.cfg'
+
+echo "Activate serial0"
+sudo sh -c 'echo "console=ttyS0,115200" >> /mnt/sda1/boot/cmdline.txt'
+sudo sh -c 'echo "enable_uart=1" >> /mnt/sda1/boot/config.txt'
+# sudo mkdir /mnt/sda1/etc
+# sudo cp /etc/inittab /mnt/sda1/etc/inittab
+# sudo sh -c 'echo "ttyS0::respawn:/sbin/getty -L 115200 /dev/ttyS0 vt100" >> /mnt/sda1/etc/inittab'
